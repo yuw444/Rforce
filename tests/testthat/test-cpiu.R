2839564473f7cc8e7483e3c6bd9ef3cc03992996
yuw444 <yuw926@icloud.com>
2024-03-20 06:49:41 -0500

    add more precise testing

--- tests/testthat/test-cpiu.R
+++ tests/testthat/test-cpiu.R
@@ -4,22 +4,32 @@ test_that("cpiu", {
     verbose = FALSE
   )
 
-  df_train <- manual_censoring(
+  df_train_admin <- admin_censoring(
     data_list$dataset,
     0.99
   )
 
-  df_train <- random_censoring(
+  expect_warning(df_train <- random_censoring(
     data_list$dataset,
     0.9,
     verbose = TRUE
+  ))
+
+  expect_equal(sum(df_train$Status ==1 )/200, 0.86)
+
+  df_train <- random_censoring(
+    data_list$dataset,
+    0.73,
+    verbose = TRUE
   )
 
-  sum(df_train$Status ==1 )/200
+  expect_equal(sum(df_train$Status ==1 )/200, 0.73)
 
   hist(df_train$X)
 
   length_cpius <- diff(quantile(c(0,df_train$X, max(df_train$X) + 0.01), probs = seq(0, 1, length.out = 10 + 1)))
+
+  expect_gte(sum(length_cpius), max(df_train$X))
 
   df_cpiu_wide <- patients_to_cpius(
     df_train,
@@ -29,6 +39,8 @@ test_that("cpiu", {
     wide_format = TRUE
   )
 
+  expect_gt(as.numeric(df_cpiu_wide$auxiliaryFeatures[2, 5+2]), 0)
+
   df_cpiu_wide_nowt <- patients_to_cpius(
     df_train,
     length_cpius,
@@ -37,5 +49,6 @@ test_that("cpiu", {
     wide_format = TRUE
   )
 
+  expect_equal(as.numeric(df_cpiu_wide_nowt$auxiliaryFeatures[2, 5+2]),0)
 
 })
