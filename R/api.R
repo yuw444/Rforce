f296e3c030fc06316d407b80d876c934d6e11807
yuw444 <yuw926@icloud.com>
2025-11-19 11:03:08 -0600

    `Rforce` related utils moved to utils.R

--- R/api.R
+++ R/api.R
@@ -57,7 +57,7 @@ RC_ReturnList <- function() {
 #' @importFrom sjmisc to_dummy
 #' @param design_matrix_Y data.frame that includes design matrix and response(number of events at each CPIU)
 #' @param auxiliary_features data.frame that includes Id, X, Status, pseudo-risktime-at-duration
-#'
+#' 
 
 Rforce <- function(
     design_matrix_Y,
@@ -136,84 +136,4 @@ Rforce <- function(
 
   class(rforce_obj) <- "Rforce"
   return(rforce_obj)
-}
-
-
-#' Predict function for Rforce
-#' @useDynLib Rforce, .registration=TRUE
-#' @export
-#' @param forest An Rforce forest object
-#' @param designMatrix A matrix of design features for prediction
-#' @return A list containing the predicted values
-#'
-predict.Rforce <- function(forest, designMatrix) {
-  if (!inherits(forest, "Rforce")) {
-    stop("forest must be an Rforce object.")
-  }
-
-  if (!is.data.frame(designMatrix) && !is.matrix(designMatrix)) {
-    stop("designMatrix must be a data frame or matrix.")
-  }
-
-  .Call(
-    "R_ForestPredict",
-    forest$`_external_forest_C_Ptr`,
-    as.matrix(designMatrix)
-  )
-}
-
-
-#' saveRDS function for Rforce
-#' @useDynLib Rforce, .registration=TRUE
-#' @export
-#' @param object An Rforce object
-#' @param file A character string giving the name of the file where the object should be saved
-saveRforce <- function(forest, file, ...) {
-  if (!inherits(forest, "Rforce")) {
-    stop("object must be an Rforce object.")
-  }
-
-  if (!is.character(file) || length(file) != 1) {
-    stop("file must be a single character string.")
-  }
-
-  .Call(
-    "R_SaveRforce",
-    forest$`_external_forest_C_Ptr`,
-    as.character(file)
-  )
-  forest1 <- forest
-  forest1$`_external_forest_C_Ptr` <- NULL
-  saveRDS(forest1, paste0(file, "/Rforce.rds"), ...)
-  rm(forest1)
-}
-
-#' loadRforce function for Rforce
-#' @useDynLib Rforce, .registration=TRUE
-#' @export
-#' @param path A character string giving the name of the path from which the object should be loaded
-loadRforce <- function(path) {
-  if (!is.character(path) || length(path) != 1) {
-    stop("file must be a single character string.")
-  }
-
-  forest <- readRDS(paste0(path, "/Rforce.rds"))
-  if (!inherits(forest, "Rforce")) {
-    stop("The loaded object is not an Rforce object.")
-  }
-
-  forest_path <- list.dirs(path, full.names = TRUE, recursive = FALSE)
-
-  if(length(forest_path) != 1) {
-    stop("The specified path does not contain a valid Rforce directory structure.")
-  }
-
-  forest$`_external_forest_C_Ptr` <- .Call(
-    "R_LoadRforce",
-    as.character(forest_path)
-  )
-  if (is.null(forest$`_external_forest_C_Ptr`)) {
-    stop("Failed to load the Rforce object from the specified path.")
-  }
-  return(forest)
 }
